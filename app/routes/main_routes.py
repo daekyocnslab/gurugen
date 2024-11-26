65a2f631b030177b386903c105cd01992d58a0a4
김우현 <woohyun_kim@daekyo.co.kr>
2024-11-26 11:50:18 +0900

    파비콘 추가 및 비디오 출력 로직 수정

--- app/routes/main_routes.py
+++ app/routes/main_routes.py
@@ -17,9 +17,34 @@ def get_video_files(file_name):
     """특정 파일 이름을 기반으로 6개의 영상 파일 목록 반환"""
     # config.VIDEO_FOLDER에서 file_name과 일치하는 파일들을 확인
     return [
-        f"{file_name}_cam{i}.mp4" for i in range(1, 7)
-        if os.path.exists(os.path.join(config.VIDEO_FOLDER, f"{file_name}_cam{i}.mp4"))
+        f"{file_name}_cam{i}.webm"
+        for i in range(1, 7)
+        if os.path.exists(os.path.join(config.VIDEO_FOLDER, f"{file_name}_cam{i}.webm"))
     ]
+
+@main_routes.route('/video/list/<file_name>', methods=['GET'])
+def video_list(file_name):
+    """파일 이름을 기반으로 관련 비디오 파일 목록 반환"""
+    video_files = get_video_files(file_name)
+    return jsonify(video_files)
+
+@main_routes.route('/video/<path:filename>', methods=['GET'])
+def video(filename):
+    """비디오 파일 반환"""
+    try:
+        return send_from_directory(config.VIDEO_FOLDER, filename)
+    except FileNotFoundError:
+        return "File not found", 404
+
+@main_routes.route('/video/exists/<path:filename>', methods=['GET'])
+def video_exists(filename):
+    """동영상 파일 존재 여부 확인"""
+    real_folder = os.path.realpath(config.VIDEO_FOLDER)
+    file_path = os.path.join(real_folder, filename)
+    if os.path.exists(file_path):
+        return jsonify({"exists": True}), 200
+    else:
+        return jsonify({"exists": False}), 404
 
 @main_routes.route('/')
 def main_page():
@@ -32,21 +57,6 @@ def report_page(report_id):
     if not report_data:
         return jsonify({"error": "Report not found"}), 404
     return jsonify(report_data)
-
-@main_routes.route('/video/<path:filename>')
-def video(filename):
-    return send_from_directory(config.VIDEO_FOLDER, filename)
-
-@main_routes.route('/video_page/<string:file_name>')
-def video_page(file_name):
-    try:
-        video_files = get_video_files(file_name)
-        if video_files:
-            return render_template('videos.html', video_files=video_files)
-        else:
-            return "Videos not found", 404
-    except Exception as e:
-        return f"Error: {e}", 500
 
 @main_routes.route('/api/insert', methods=['POST'])
 def insert_json_data():
